1fc61b34fbe88a75de519e2c4a792403e8192f1d
Nyamso <msosarehema@outlook.com>
2024-02-01 15:03:54 +0200

    update tasks

--- 0x06-unittests_in_js/.eslintrc.js
+++ 0x06-unittests_in_js/.eslintrc.js
@@ -1,51 +1,32 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended'
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
     ],
-    'overrides': [
+    "overrides": [
         {
-            'env': {
-                'node': true
+            "env": {
+                "node": true
             },
-            'files': [
-                '.eslintrc.{js,cjs}'
+            "files": [
+                ".eslintrc.{js,cjs}"
             ],
-            'parserOptions': {
-                'sourceType': 'script'
+            "parserOptions": {
+                "sourceType": "script"
             }
         }
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    'plugins': [
-        '@typescript-eslint',
-        'react'
+    "plugins": [
+        "react"
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
+    "rules": {
     }
-};
+}
