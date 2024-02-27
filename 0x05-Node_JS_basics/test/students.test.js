46cfcbbe02ffb08d705abfb53912b66ea6fdedcf
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2024-02-27 15:36:32 +0200

    working

--- 0x05-Node_JS_basics/test/students.test.js
+++ 0x05-Node_JS_basics/test/students.test.js
@@ -1,1 +1,24 @@
+/* eslint-disable indent */
+/* eslint-disable semi */
+/* eslint-disable n/handle-callback-err */
+/* eslint-disable no-undef */
+import app from '../full_server/server.js';
+import { use, request, expect } from 'chai';
+import chaiHttp from 'chai-http';
+process.argv[2] = './database.csv';
 
+use(chaiHttp);
+
+describe('Server!', () => {
+  it('welcomes user to the api', (done) => {
+    request(app)
+      .get('/students/CS')
+      .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.equals(
+              'List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie'
+          );
+          done();
+      });
+  });
+})
