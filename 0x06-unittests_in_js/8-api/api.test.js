1f4881b986212cf67875fb69a0782353eeca0b21
Rehema Msosa <105172957+Nyamso@users.noreply.github.com>
2024-05-02 22:31:53 +0200

    Update

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,7 +3,6 @@ const { expect } = require('chai');
 
 describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
-
   it('GET / returns correct response', (done) => {
     request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
